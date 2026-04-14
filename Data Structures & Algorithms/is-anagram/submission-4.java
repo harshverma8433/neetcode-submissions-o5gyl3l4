7f7f65ff484619e9e4c0ce60da39d64a90d69a89
harshverma8433 <93326986+harshverma8433@users.noreply.github.com>
2026-04-14 15:06:51 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,65 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // HashMap<Character,Integer> map1 = new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(map.containsKey(s.charAt(i))){
+        //         int v = map.get(s.charAt(i));
+        //         map.put(s.charAt(i),v+1);
+        //     }else{
+        //         map.put(s.charAt(i),1);
+
+        //     }
+        // }
+        
+      
+        
+        // for(int i=0;i<t.length();i++){
+        //     if(map1.containsKey(t.charAt(i))){
+        //         int v = map1.get(t.charAt(i));
+        //         map1.put(t.charAt(i),v+1);
+        //     }else{
+        //         map1.put(t.charAt(i),1);
+
+        //     }
+        // }
+        
+        //   if(map.size() != map1.size()){
+        //     return false;
+        // }
+        
+            
+
+        
+
+        //  for(Map.Entry<Character,Integer> entry : map.entrySet()){
+        //     char key = entry.getKey();
+        //     if(!map1.containsKey(key)){
+        //         return false;
+        //     }else{
+        //         int val = entry.getValue();
+        //         int val1 = map1.get(key);
+        //         if(val != val1){
+        //             return false;
+        //         }
+        //     }
+
+
+        // }
+
+        // return true;
+
+        String[] arr1 = s.split("");
+        String[] arr2 = t.split("");
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+       
+       s = String.join("" , arr1);
+       t = String.join("" , arr2);
+       
+        if(s.equals(t)){
+            return true;
+        }
+        return false;
+    }
+}
