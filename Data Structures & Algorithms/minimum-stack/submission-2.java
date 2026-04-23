cea916e62947ca02eda0a4891df07434b587ec62
harshverma8433 <93326986+harshverma8433@users.noreply.github.com>
2026-04-24 00:21:13 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,44 @@
+class MinStack {
+
+    Stack<Integer> stack;
+
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        // int min = stack.peek();
+        // for(int i=0;i<stack.size();i++){
+        //     if(stack.get(i) < min){
+        //         min = stack.get(i);
+        //     }
+        // }
+        // return min;
+        Stack<Integer> sec_stack = new Stack<>();
+         int min = stack.peek();
+
+        while (!stack.isEmpty()) {
+            min = Math.min(min, stack.peek());
+            sec_stack.push(stack.pop());
+        }
+
+        while (!sec_stack.isEmpty()) {
+            stack.push(sec_stack.pop());
+        }
+
+        return min;
+    }
+}
